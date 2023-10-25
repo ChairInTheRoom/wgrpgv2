957aabae912d95aea894932352b4644f292105cf
Niwatori401 <cjackson@firemail.cc>
2023-10-25 15:22:46 +0900

    Update launch scripts

--- ~run_container.bat
+++ ~run_container.bat
@@ -1,2 +1,4 @@
+@echo Launching container...
 docker volume create dbwgrpg
-docker run --name=wgrpg --rm -p8401:80 -v dbwgrpg:/opt/lampp/var/mysql/ niwatori401/wgrpg
+docker run --name=wgrpg --rm -d -p8401:80 -v dbwgrpg:/opt/lampp/var/mysql/ niwatori401/wgrpg
+@echo Finished. You can play the game at: http://localhost:8401/wgrpg
